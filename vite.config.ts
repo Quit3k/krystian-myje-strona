251bc0002a2a97942881f796e04c3cfb2eb192eb
Eryk <erykkokoczkaa@gmail.com>
2025-09-02 18:41:14 +0200

    Poprawiono index.html i konfigurację Vite (base path)

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/classic-cars-strona/", 
+  // ZMIANA: Poprawna ścieżka do nowego repozytorium
+  base: "/krystian-myje-strona/", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
